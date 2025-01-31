88eb31dacb5d75fc218c1eece27c4f35cf543bf7
Mats De Beer <mde-beer@f0r2s2.codam.nl>
2025-01-31 16:24:29 +0100

    changed around some names and definitions, to run a command, everything should be figured out *beforehand*

--- inc/minishell.h
+++ inc/minishell.h
@@ -16,13 +16,15 @@
 # define DEFAULT_PROMPT "Σ:3 "
 # define DEFAULT_CONTINUATION "> "
 # define HANDLER_COUNT 2
-
+//	prototype inclusions
+# include <stdbool.h>	//	bool
+# include <sys/types.h> //	pid_t
 //	typedefs
 typedef struct s_tokenisation_output	t_tokenisation;
 typedef struct s_individual_token		t_token;
 typedef struct s_builtin_utility		t_builtin;
-typedef struct s_redirections			t_redirections;
-typedef struct s_simple_command			t_job;
+typedef struct s_execve_environments	t_execve;
+typedef struct s_simple_command			t_command;
 
 //	enums
 enum e_tokenisation_result_type
@@ -57,49 +59,84 @@ struct s_builtin_utility
 	const char	*name;
 	int			(*function)(char **);
 };
-struct s_redirections
+struct s_execve_environments
 {
-	int	stdin;
-	int	stdout;
-	int	stderr;
+	bool	builtin;
+	int		in;
+	int		out;
+	int		err;
+	char	*path;
+	char	**argv;
 };
 struct s_simple_command
 {
-	char			**argv;
-	t_redirections	redirections;
+	unsigned int	count;
+	t_execve		*commands;
+	int				infile;
+	int				outfile;
+	int				errfile;
 };
 
 //	function prototypes
 //	//	porcelain
-
-//	//	builtins
-int
-	builtin(
-		char **argv
-		);	// FILE: binlocation.c
-			// checks wether something _is_ a builtin, and if it is, runs it
-int
+//	//	job-control
+int			
 	command(
 		const char **argv
 		);	// FILE: binlocation.c
 			// attempts to find a file by the name of bin somewhere in $PATH
-int
+pid_t		
+	boss_baby(
+		t_execve parameters,
+		int close_in_child
+		);	// FILE: execution.c
+			// execute_in_child() => executive_child() => boss_baby()
+			// forks process, child execve's, parent returns pid of child
+void		
+	set_execve_fds(
+		t_execve *parameters,
+		int in,
+		int out,
+		int err
+		);	// FILE: execution.c
+_Noreturn void
+	iterative_pipes(
+		t_command command
+		);	// FILE: execution.c
+			// despite the name, this will also work for non-pipe executions (i
+			// hope)
+int			
+	pipeline(
+		t_command command
+		);	// FILE: execution.c
+			// returns the status of the final command ran
+_Noreturn void
+	do_execve(
+		t_execve parameters
+		);	// FILE: execution.c
+//	//	builtins
+int			
+	builtin(
+		char **argv
+		);	// FILE: binlocation.c
+			// checks wether something _is_ a builtin, and if it is, runs it
+int			
 	builtin_echo(
 		char **argv
 		);	// FILE: builtins.c
-int
+int			
 	builtin_pwd(
 		char **argv
 		);	// FILE: builtins.c
-int
+int			
 	builtin_export(
 		char **argv
 		);	// FILE: builtins.c
-int
+int			
 	builtin_unset(
 		char **argv
 		);	// FILE: builtins.c
-int
+int			
 	builtin_env(
 		char **argv
 		);	// FILE: builtins.c
--- src/execution.c
+++ src/execution.c
@@ -10,31 +10,38 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <minishell.h>
 #include <ft_env.h>
 #include <unistd.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <sys/wait.h>
 #include <errno.h>
 
-typedef struct s_execve_environments	t_execve;
-typedef struct s_simple_command			t_command;
-struct s_execve_environments
+_Noreturn void
+	do_execve(
+t_execve parameters
+)
 {
-	int		in;
-	int		out;
-	int		err;
-	char	*path;
-	char	**argv;
-};
-struct s_simple_command
+	dup2(parameters.in, STDIN_FILENO);
+	dup2(parameters.out, STDOUT_FILENO);
+	dup2(parameters.err, STDERR_FILENO);
+	if (parameters.builtin)
+		exit(builtin(parameters.argv));
+	exit(execve(parameters.path, parameters.argv, (char *const *)borrow_env()));
+}
+
+void
+	set_execve_fds(
+t_execve *parameters,
+int in,
+int out,
+int err
+)
 {
-	unsigned int	count;
-	t_execve		*commands;
-	int				infile;
-	int				outfile;
-	int				errfile;
-};
+	parameters->in = in;
+	parameters->out = out;
+	parameters->err = err;
+}
 
 pid_t
 	boss_baby(
@@ -48,58 +55,53 @@ int close_in_child
 		return (-1);
 	if (id)
 		return (id);
-	dup2(parameters.in, STDIN_FILENO);
-	dup2(parameters.out, STDOUT_FILENO);
-	dup2(parameters.err, STDERR_FILENO);
 	if (close_in_child != -1)
 		close(close_in_child);
-	exit(execve(parameters.path, parameters.argv, (char *const *)borrow_env()));
+	do_execve(parameters);
 }
 
-int
-	pipeline(
-t_command	command
+_Noreturn void
+	iterative_pipes(
+t_command command
 )
 {
-	const int		in = command.infile;
-	int				pfd[2];
-	int				status;
-	unsigned int	i;
-	pid_t			id;
+	const int	in = command.infile;
+	int			pfd[2];
 
-	id = fork();
-	if (id < 0)
-		return (-1);
-	if (id)
-	{
-		waitpid(id, &status, 0);
-		while (errno != ECHILD)
-			waitpid(0, NULL, 0);
-		close(in);
-		close(command.outfile);
-		return (WEXITSTATUS(status));
-	}
-	i = -1;
-	while (++i < command.count - 1)
+	while (command.commands + 1)
 	{
 		if (pipe(pfd))
 		{
 			dup2(-1, in);
 			continue ;
 		}
-		command.commands[i].in = in;
-		command.commands[i].out = pfd[1];
-		command.commands[i].err = command.errfile;
-		id = boss_baby(command.commands[i], pfd[0]);
-		close(pfd[1]);
-		if (id < 0)
+		set_execve_fds(command.commands, in, pfd[1], command.errfile);
+		if (boss_baby(*command.commands++, pfd[0]) < 0)
 			close(pfd[0]);
+		close(pfd[1]);
 		dup2(pfd[0], in);
 	}
-	dup2(in, STDIN_FILENO);
-	dup2(command.outfile, STDOUT_FILENO);
-	dup2(command.errfile, STDERR_FILENO);
-	exit(execve(command.commands[i].path,
-			command.commands[i].argv,
-			(char *const *)borrow_env()));
+	set_execve_fds(command.commands, in, command.outfile, command.errfile);
+	do_execve(*command.commands);
+}
+
+// NOTE function does not open or close any files, this is the responsibility of
+// the calling function
+int
+	pipeline(
+t_command	command
+)
+{
+	int				status;
+	pid_t			id;
+
+	id = fork();
+	if (id < 0)
+		return (-1);
+	if (!id)
+		iterative_pipes(command);
+	waitpid(id, &status, 0);
+	while (errno != ECHILD)
+		waitpid(0, NULL, 0);
+	return (WEXITSTATUS(status));
 }
